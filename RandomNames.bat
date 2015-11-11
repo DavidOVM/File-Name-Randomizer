daa85e8de995140e2d6a4c383c5f2d7300b42829
DavidOVM <davidovm94@gmail.com>
2015-11-11 20:32:56 +0100

    Save in translation file the new and previous names

--- RandomNames.bat
+++ RandomNames.bat
@@ -1,7 +1,9 @@
 @ECHO OFF
-
-REM Randomly renames every file in a directory.
-
+REM Authord David Ortiz-Vallejuelo
+REM Feel free to use this code or to change anything
+REM
+REM adds a random number to the name of every file in the directory.
+SET TranslationFile=__Translation.txt
 SETLOCAL EnableExtensions EnableDelayedExpansion
 
 	REM Rename files
@@ -16,11 +18,14 @@ SETLOCAL EnableExtensions EnableDelayedExpansion
 		ECHO Aborting :( .
 		GOTO :EOF
 	)
+	ECHO Original Name/Random Name > %TranslationFile%
+	ECHO ------------------------- >> %TranslationFile%
 	FOR /F "tokens=*" %%A IN ('DIR /A:-D /B') DO (
 		IF NOT %%A==%~nx0 (
 			SET Use=%%~xA
 			SET Name=%%~nA
 			SET NewName=!RANDOM!-!Name!!Use!
+			ECHO %%A/!NewName!>> %TranslationFile%
 			RENAME "%%A" "!NewName!"
 			
 		)
